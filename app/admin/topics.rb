5e52d96c8c45c8ea58b6abbd6ccfbd65d62e50d8
Raghu Betina <raghu@firstdraft.com>
2018-11-29 02:28:23 +0000

    Whitelist columns in Topic for ActiveAdmin

--- app/admin/topics.rb
+++ app/admin/topics.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Topic do
+
+ permit_params :csi_section, :title, :engineering_discipline
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
